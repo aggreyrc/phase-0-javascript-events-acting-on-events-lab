fa29941d751f5d675fb51b609034f1d4455daf12
aggreyrc <aggreyronoh@gmail.com>
2024-07-04 21:36:52 +0300

    Javascript-events-acting-on-events-lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.background = "#FF69B4";
+dodger.style.bottom = "0px";
+
+
+document.addEventListener("keydown", function(event){
+    if(event.key === "ArrowLeft"){
+      moveDodgerLeft();
+    } 
+});
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(event){
+    if(event.key === "ArrowRight"){
+      moveDodgerRight();
+    } 
+});
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if(left < 360){
+        dodger.style.left = `${left + 1}px`;
+    } 
+}
+
